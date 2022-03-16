2473778b0b2307b9f9848ecb14bada3a24e28240
neshi00 <luca.ascione85@gmail.com>
2022-03-16 17:37:35 +0100

    relazione molti a molti model + tabella ponte (tag_post)

--- laravel-boolpress/app/Tag.php
+++ laravel-boolpress/app/Tag.php
@@ -5,5 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model {
+
+    public function posts() {
+        return $this->belongsToMany("App\Post");
+    }
     
 }
