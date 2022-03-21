b9c1ffba31b71e30f253a281a3c8165db867ddb5
neshi00 <luca.ascione85@gmail.com>
2022-03-21 16:20:13 +0100

    configura e attiva vue router su vue

--- laravel-boolpress/resources/js/vue.js
+++ laravel-boolpress/resources/js/vue.js
@@ -16,6 +16,7 @@ window.Vue = require("vue");
 //     require("./components/ExampleComponent.vue").default
 // );
 import App from "./views/App.vue";
+import router from "./router";
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -26,4 +27,5 @@ import App from "./views/App.vue";
 const app = new Vue({
     el: "#appVue",
     render: (h) => h(App),
+    router: router,
 });
